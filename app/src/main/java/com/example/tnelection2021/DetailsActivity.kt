fe01b8312ef906fca15eb15da697cfcba5f1bfb1
bharani <bharani19797@gmail.com>
2021-03-23 16:38:27 +0530

    pdf doenloaded

--- app/src/main/java/com/example/tnelection2021/DetailsActivity.kt
+++ app/src/main/java/com/example/tnelection2021/DetailsActivity.kt
@@ -4,17 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.example.tnelection2021.PdfDownload.DownloadFile
 import com.example.tnelection2021.model.CandidateDetails
 import kotlinx.android.synthetic.main.activity_details.*
 
 
-class DetailsActivity : AppCompatActivity()
+public class DetailsActivity : AppCompatActivity()
 {
     lateinit var mCandidateDetails : CandidateDetails
     override fun onCreate(savedInstanceState: Bundle?)
     {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
@@ -55,6 +58,14 @@ class DetailsActivity : AppCompatActivity()
         val intent = Intent(this, VideoPlayer::class.java)
         intent.putExtra("candidate_details", mCandidateDetails)
         startActivity(intent)
+
+    }
+
+    fun downloadPdf(view: View) {
+       val path= mCandidateDetails.candidate_pdf;
+        Toast.makeText(this,path,Toast.LENGTH_LONG).show();
+         DownloadFile(this).execute(path,mCandidateDetails.candidate_name+"_"+mCandidateDetails.candidate_party+"_.pdf")
+
 
     }
 
